743a91752d7aaf96f2ea73bc598a5c1bfa30df35
Braden Walters <vc@braden-walters.info>
2012-02-26 00:34:19 -0500

    Translatable forms

--- karteikarten/forms.py
+++ karteikarten/forms.py
@@ -14,22 +14,23 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from django import forms
+from django.utils.translation import ugettext as _
 
 #
 # Form displayed on login page.
 #
 class LoginForm(forms.Form):
-	username = forms.CharField(max_length = 30)
-	password = forms.CharField(widget = forms.PasswordInput)
+	username = forms.CharField(max_length = 30, label = _('username'))
+	password = forms.CharField(widget = forms.PasswordInput, label = _('password'))
 
 #
 # Form displayed on registration page.
 #
 class RegisterForm(forms.Form):
-	username = forms.CharField(max_length = 30)
-	password = forms.CharField(widget = forms.PasswordInput)
-	repeat_password = forms.CharField(widget = forms.PasswordInput)
-	email = forms.EmailField()
+	username = forms.CharField(max_length = 30, label = _('username'))
+	password = forms.CharField(widget = forms.PasswordInput, label = _('password'))
+	repeat_password = forms.CharField(widget = forms.PasswordInput, label = _('repeat-password'))
+	email = forms.EmailField(label = _('email'))
 
 	def clean_repeat_password(self):
 		if self.cleaned_data['password'] != self.cleaned_data['repeat_password']:
@@ -40,34 +41,34 @@ class RegisterForm(forms.Form):
 # Form displayed on password recovery email request page.
 #
 class RecoverPasswordForm(forms.Form):
-	username = forms.CharField(max_length = 30)
-	email = forms.EmailField()
+	username = forms.CharField(max_length = 30, label = _('username'))
+	email = forms.EmailField(label = _('email'))
 
 #
 # Form displayed on password reset form.
 #
 class RecoverResetPasswordForm(forms.Form):
-	username = forms.CharField(max_length = 30)
-	password = forms.CharField(widget = forms.PasswordInput)
-	repeat_password = forms.CharField(widget = forms.PasswordInput)
+	username = forms.CharField(max_length = 30, label = _('username'))
+	password = forms.CharField(widget = forms.PasswordInput, label = _('password'))
+	repeat_password = forms.CharField(widget = forms.PasswordInput, label = _('repeat-password'))
 
 	def clean_repeat_password(self):
 		if self.cleaned_data['password'] != self.cleaned_data['repeat_password']:
-			raise forms.ValidationError('The two passwords do not match.')
+			raise forms.ValidationError(_('passwords-dont-match'))
 		return self.cleaned_data
 
 #
 # Form displayed when selecting set to edit
 #
 class SelectSetForm(forms.Form):
-	card_set = forms.ChoiceField()
+	card_set = forms.ChoiceField(label = _('card-set'))
 
 	def __init__(self, card_set_choices, is_edit_set, values = None):
 		super(SelectSetForm, self).__init__(values)
 		card_set_names = []
 		#If edit set form, display option to create a new set
 		if is_edit_set:
-			card_set_names.append(['new', '[Create New Set]'])
+			card_set_names.append(['new', '[' + _('create-new-set') + ']'])
 		#Copy set id and name for value and text of choices
 		for i, item in enumerate(card_set_choices):
 			card_set_names.append([str(item.pk), item.name])
@@ -78,9 +79,9 @@ class SelectSetForm(forms.Form):
 # Form displayed when beginning a study session.
 #
 class RunOptionsForm(forms.Form):
-	card_set = forms.ChoiceField()
-	study_type = forms.ChoiceField(widget = forms.RadioSelect, choices = [['normal', 'Normal'], ['single_box', 'Practice Single Box'], ['no_box', 'Practice Cards Currently in No Box']])
-	card_box = forms.ChoiceField(required = False)
+	card_set = forms.ChoiceField(label = _('card-set'))
+	study_type = forms.ChoiceField(widget = forms.RadioSelect, choices = [['normal', _('study-type-normal')], ['single_box', _('study-type-single-box')], ['no_box', _('study-type-no-box')]], label = _('study-type'))
+	card_box = forms.ChoiceField(required = False, label = _('card-box'))
 
 	def __init__(self, card_set_choices, card_box_choices, values = None):
 		super(RunOptionsForm, self).__init__(values)
@@ -100,22 +101,22 @@ class RunOptionsForm(forms.Form):
 # Form displayed when creating and editing card sets.
 #
 class EditSetForm(forms.Form):
-	name = forms.CharField(max_length = 60)
+	name = forms.CharField(max_length = 60, label = _('name'))
 
 #
 # Form displayed when creating and editing card boxes.
 #
 class EditBoxForm(forms.Form):
-	name = forms.CharField(max_length = 60)
-	review_frequency = forms.IntegerField(label = "Review Frequency (In Days)")
+	name = forms.CharField(max_length = 60, label = _('name'))
+	review_frequency = forms.IntegerField(label = _('review-frequency-in-days'))
 
 #
 # Form displayed when creating and editing cards.
 #
 class EditCardForm(forms.Form):
-	front = forms.CharField(widget = forms.Textarea({'rows' : 5}))
-	back = forms.CharField(widget = forms.Textarea({'rows' : 5}))
-	card_box = forms.ChoiceField()
+	front = forms.CharField(widget = forms.Textarea({'rows' : 5}), label = _('front'))
+	back = forms.CharField(widget = forms.Textarea({'rows' : 5}), label = _('back'))
+	card_box = forms.ChoiceField(label = _('card-box'))
 
 	def __init__(self, card_box_choices, values = None):
 		super(EditCardForm, self).__init__(values)
@@ -124,6 +125,6 @@ class EditCardForm(forms.Form):
 		for i, item in enumerate(card_box_choices):
 			card_box_names.append([str(item.pk), item.name])
 		#Create no box option
-		card_box_names.append(['0', '[No Box]'])
+		card_box_names.append(['0', '[' + _('no-box') + ']'])
 		#Provide choices for card box choice field
 		self.fields['card_box'].choices = card_box_names
