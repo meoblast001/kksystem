9a6806c95eda6892b27725255df718444b99d62e
Braden Walters <vc@braden-walters.info>
2014-06-02 23:47:19 -0400

    Card, Cardbox, and Cardset models with relations and simple validations

---
+++ app/models/cardbox.rb
@@ -0,0 +1,22 @@
+# Copyright (C) 2014 Braden Walters
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class Cardbox < ActiveRecord::Base
+  belongs_to :cardset
+  has_many :cards
+
+  validates_presence_of :user_id, :cardset_id, :name
+  validates :review_frequency, :numericality => { :greater_than => 0 }
+end
