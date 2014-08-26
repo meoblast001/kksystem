68a35064cc78725a65206d8dbb1b1f582592ed62
Braden Walters <vc@braden-walters.info>
2014-08-25 23:45:31 -0400

    HTTP Accept-Language support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,13 @@ class ApplicationController < ActionController::Base
   #Prevent CSRF attacks by raising an exception.
   #For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :setLocale
+
+  private
+
+  def setLocale
+    I18n.locale = http_accept_language.compatible_language_from(
+      I18n.available_locales)
+  end
 end
